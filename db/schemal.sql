0f8ab4df5ffb7f4eea10f1755ba2a386b8d0aed6
jasonmueller <jmuel9999@gmail.com>
2021-06-18 19:06:09 -0500

    schema.sql finished

--- db/schemal.sql
+++ db/schemal.sql
@@ -8,8 +8,16 @@ CREATE TABLE department (
 );
 
 CREATE TABLE role (
-    id AUTO_INCREMENT PRIMARY KEY,
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30), NOT NULL ,
     salary DECIMAL, NOT NULL,
     department_id INT
 );
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name: VARCHAR(30),
+    role_id: INTEGER,
+    manager_id: INTEGER 
+)
